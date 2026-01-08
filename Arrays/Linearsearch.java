b2e0d823aa3e00f2d6b7d835ccf91908496f0803
Yug Kukreja <yugkukreja808@gmail.com>
2026-01-08 19:00:45 +0530

    LinearSearch

--- Arrays/Linearsearch.java
+++ Arrays/Linearsearch.java
@@ -1,29 +1,29 @@
 import java.util.*;
 public class Linearsearch{
-    // Linear Search 
-    public static int linearsearch(int Marks[],int key){
+    public static int linearsearch(int Marks[], int key){
         for(int i=0;i<Marks.length;i++){
-            if (Marks[i]==key){
-
+            if(Marks[i]==key)
                 return i;
             }
+            return -1;
         }
-                return -1;
-    }
+               
+    
     public static void main(String args[]){
-
+        
         int Marks[] = {1,2,3,4,5,6,7,8,9,10};
         Scanner sc = new Scanner (System.in);
-        System.out.print("Enter the number your wnat to find : ");
+        System.out.print("Enter the number you want to find out : ");
         int key = sc.nextInt();
+
         int Index = linearsearch(Marks,key);
 
-        if (Index == -1){
+        if(Index==-1){
             System.out.print("Not Found");
         }
         else{
-            System.out.print("The Number is present on index : " + Index);
+            System.out.print("The Index of the number is : " + Index);
         }
-          
+
     }
 }
