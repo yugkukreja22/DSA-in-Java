796cba9d47f7f98e36c96fed2e3a46b04ce3d439
yugkukreja22 <yugkukreja808@gmail.com>
2026-01-09 12:40:43 +0530

    Binary search function

--- Arrays/Binarysearch.java
+++ Arrays/Binarysearch.java
@@ -1,13 +1,8 @@
 import java.util.*;
 
 public class Binarysearch{
-    public static void main(String args[]){
 
-        int numbers[] = {1,2,3,4,5,6,7,8,9,10};
-        Scanner sc = new Scanner (System.in);
-        System.out.print("Enter the element you want to find : ");
-        int key = sc.nextInt();
-
+    public static int Binarysearch(int numbers[] , int key){
         int n = numbers.length;
         int start = 0;
         int end = n-1;
@@ -16,8 +11,8 @@ public class Binarysearch{
              int mid = (start + end)/2;
 
              if(numbers[mid]==key){
-                System.out.print("The number is found at position"+ ":" + (mid + 1));
-                break;
+                int result = (mid + 1);
+                return result;
              }
              else if(numbers[mid]>key){
                 end = mid - 1 ;
@@ -25,6 +20,23 @@ public class Binarysearch{
              else if(numbers[mid]<key){
                 start = mid + 1 ;
              }
+        }
+              return -1;
+    }
+    public static void main(String args[]){
+
+        int numbers[] = {1,2,3,4,5,6,7,8,9,10};
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the element you want to find : ");
+        int key = sc.nextInt();
+
+        int solution = Binarysearch(numbers,key);
+
+        if(solution == -1){
+            System.out.print("The number is not found");
+        }
+        else{
+            System.out.print("The number is found at position :" + " " + solution);
         }
     }
 }
