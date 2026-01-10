8281462961b46a613a56e2bd7d665873a34d9cd9
yugkukreja22 <yugkukreja808@gmail.com>
2026-01-10 13:35:59 +0530

    Subarrays

---
+++ Arrays/Subarrays.java
@@ -0,0 +1,23 @@
+public class subarrays{
+
+    // Subarrays 
+    public static void main(String args[]){
+        
+        int numbers[] = {1,2,3,4,5,6,7,8,9,10};
+        int count = 0;
+
+        for(int i=0;i<numbers.length;i++){
+            int Start = i;
+        for(int j=i;j<numbers.length;j++){
+            int End = j;
+        for(int k=Start;k<=End;k++){
+            System.out.print(numbers[k] + " ");
+        }
+        count ++;
+        System.out.println();
+        }
+        System.out.println();
+        }
+        System.out.print("The total number of subarrays are : " + count);
+    }
+}
